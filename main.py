033a1e2b9284fb8110da37d3ca300e0fc31e7de1
GemerLiew <67371687+GemerLiew@users.noreply.github.com>
2023-09-03 13:37:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ TG_USER_ID = os.environ.get('TGUSERID')
 
 def checkin(email=os.environ.get('EMAIL'), password=os.environ.get('PASSWORD'),
             base_url=os.environ.get('BASE_URL'), ):
-    #email = email.split('@')
-    #email = email[0] + '%40' + email[1]
+    email = email.split('@')
+    email = email[0] + '%40' + email[1]
     session = requests.session()
     session.get(base_url, verify=False)
     login_url = base_url + '/auth/login'
